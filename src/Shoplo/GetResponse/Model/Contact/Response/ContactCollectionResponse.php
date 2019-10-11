8e48956525a9700f45d6d2c472f168341ce37c73
adrianadamiec <adrianadamiec@gmail.com>
2019-10-11 09:12:22 +0200

    cr fixes

--- src/Shoplo/GetResponse/Model/Contact/Response/ContactCollectionResponse.php
+++ src/Shoplo/GetResponse/Model/Contact/Response/ContactCollectionResponse.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Shoplo\GetResponse\Model\Contact\Response;
 
 use Shoplo\GetResponse\Model\BaseCollectionResponse;
-use Shoplo\GetResponse\Model\Contact\Request\ContactResponse;
 
 class ContactCollectionResponse extends BaseCollectionResponse
 {
--- src/Shoplo/GetResponse/Model/Contact/Response/ContactResponse.php
+++ src/Shoplo/GetResponse/Model/Contact/Response/ContactResponse.php
@@ -2,7 +2,9 @@
 
 declare(strict_types=1);
 
-namespace Shoplo\GetResponse\Model\Contact\Request;
+namespace Shoplo\GetResponse\Model\Contact\Response;
+
+use Shoplo\GetResponse\Model\Contact\Request\ContactCampaignRequest;
 
 class ContactResponse
 {
